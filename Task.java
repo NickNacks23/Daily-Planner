eb5762b40048895c6e262a3d37f30656a3306b9e
Nicholas Garcia <nickgarcia0523@gmail.com>
2025-10-29 22:33:03 -0700

    Update Task.java

--- Task.java
+++ Task.java
@@ -9,37 +9,32 @@ public class Task {
     private final StringProperty category;
 
     public Task(String name, String date, String priority, String status, String category) {
-        this.name     = new SimpleStringProperty(name);
-        this.date     = new SimpleStringProperty(date);
+        this.name = new SimpleStringProperty(name);
+        this.date = new SimpleStringProperty(date);
         this.priority = new SimpleStringProperty(priority);
-        this.status   = new SimpleStringProperty(status);
+        this.status = new SimpleStringProperty(status);
         this.category = new SimpleStringProperty(category);
     }
-
     // Name
-    public String getName()              { return name.get(); }
-    public void   setName(String value)  { name.set(value); }
+    public String getName() { return name.get(); }
+    public void   setName(String value) { name.set(value); }
     public StringProperty nameProperty(){ return name; }
-
     // Date
-    public String getDate()               { return date.get(); }
-    public void   setDate(String value)   { date.set(value); }
-    public StringProperty dateProperty()  { return date; }
-
+    public String getDate() { return date.get(); }
+    public void   setDate(String value) { date.set(value); }
+    public StringProperty dateProperty() { return date; }
     // Priority
-    public String getPriority()                  { return priority.get(); }
-    public void   setPriority(String value)      { priority.set(value); }
-    public StringProperty priorityProperty()     { return priority; }
-
+    public String getPriority() { return priority.get(); }
+    public void   setPriority(String value) { priority.set(value); }
+    public StringProperty priorityProperty() { return priority; }
     // Status
-    public String getStatus()                { return status.get(); }
-    public void   setStatus(String value)    { status.set(value); }
-    public StringProperty statusProperty()   { return status; }
-
+    public String getStatus() { return status.get(); }
+    public void   setStatus(String value) { status.set(value); }
+    public StringProperty statusProperty() { return status; }
     // Category
-    public String getCategory()                { return category.get(); }
-    public void   setCategory(String value)    { category.set(value); }
-    public StringProperty categoryProperty()   { return category; }
+    public String getCategory() { return category.get(); }
+    public void   setCategory(String value) { category.set(value); }
+    public StringProperty categoryProperty() { return category; }
 }
 public class Task {
 
